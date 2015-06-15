110bde5cbcf50403fec980e25944c4055c8ff3b5
coderyi <517576002@qq.com>
2015-06-15 17:46:03 +0800

    Update YiRefreshFooter.h

--- Monkey/Vendor/YiRefresh/YiRefreshFooter.h
+++ Monkey/Vendor/YiRefresh/YiRefreshFooter.h
@@ -4,7 +4,8 @@
 //
 //  Created by apple on 15/3/6.
 //  Copyright (c) 2015年 coderyi. All rights reserved.
-//
+//YiRefresh is a simple way to use pull-to-refresh.下拉刷新，大道至简，最简单的网络刷新控件 
+//项目地址在：https://github.com/coderyi/YiRefresh
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
