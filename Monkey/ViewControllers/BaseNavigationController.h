97d4a7b4f33522a841d21bebcf4a8ecb4e691518
Jack <xhzengAIB@gmail.com>
2015-06-30 16:52:08 +0800

    change the statusBarStyle

---
+++ Monkey/ViewControllers/BaseNavigationController.h
@@ -0,0 +1,15 @@
+//
+//  BaseNavigationController.h
+//  Monkey
+//
+//  Created by Jack_iMac on 15/6/30.
+//  Copyright (c) 2015年 www.coderyi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@property (nonatomic, assign) UIStatusBarStyle statusBarStyle;
+
+@end
