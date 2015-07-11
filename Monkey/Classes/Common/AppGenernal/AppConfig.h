5507a0fedc75447c564bb3192bc64649d3a7aa68
coderyi <coderyi@foxmail.com>
2015-07-11 21:01:28 +0800

    调整Monkey的架构

---
+++ Monkey/Classes/Common/AppGenernal/AppConfig.h
@@ -0,0 +1,13 @@
+//
+//  AppConfig.h
+//  Monkey
+//
+//  Created by coderyi on 15/7/11.
+//  Copyright (c) 2015年 www.coderyi.com. All rights reserved.
+//
+
+#ifndef Monkey_AppConfig_h
+#define Monkey_AppConfig_h
+
+
+#endif
