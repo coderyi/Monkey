409bcbba49e1c224599ada9d4a1778c4075b6bf3
coderyi <coderyi@foxmail.com>
2015-07-11 21:02:07 +0800

    调整Monkey的架构

--- Monkey/AppDelegate.h
+++ Monkey/AppDelegate.h
@@ -7,11 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "APIEngine.h"
+#import "YiNetworkEngine.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
 
-@property (nonatomic,strong) APIEngine *apiEngine;
+@property (nonatomic,strong) YiNetworkEngine *apiEngine;
 @end
