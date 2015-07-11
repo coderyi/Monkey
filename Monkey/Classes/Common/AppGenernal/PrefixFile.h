75d188e99472707a9973cf34ac5b7b705220e774
coderyi <coderyi@foxmail.com>
2015-07-11 21:02:17 +0800

    调整Monkey的架构

---
+++ Monkey/Classes/Common/AppGenernal/PrefixFile.h
@@ -0,0 +1,27 @@
+
+//
+//  PrefixFile.h
+//  Monkey
+//
+//  Created by coderyi on 15/7/11.
+//  Copyright (c) 2015年 www.coderyi.com. All rights reserved.
+//
+
+#ifndef Monkey_PrefixFile_h
+#define Monkey_PrefixFile_h
+
+/**
+ *   header
+ */
+#import "AppDelegate.h"
+#import "YiRefreshHeader.h"
+#import "YiRefreshFooter.h"
+#import "DataSourceModel.h"
+#import "MKNetworkKit.h"
+#import "MKNetworkEngine+DefaultSet.h"
+#import "YiNetworkEngine.h"
+#import "UIImageView+WebCache.h"
+#import "NSObject+HUD.h"
+#import "NSObject+JudgeNull.h"
+
+#endif
