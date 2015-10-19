43a48aa47547d94d19370d16593ec1e6ea67ba3f
coderyi <coderyi@163.com>
2015-10-20 00:59:40 +0800

    add Swift

--- Monkey/Classes/Common/ViewControllers/LanguageViewController.m
+++ Monkey/Classes/Common/ViewControllers/LanguageViewController.m
@@ -44,9 +44,9 @@
     tableView1.delegate=self;
   
     if (_languageEntranceType==RepLanguageEntranceType) {
-        languages=@[@"JavaScript",@"Java",@"PHP",@"Ruby",@"Python",@"CSS",@"C",@"Objective-C",@"Shell",@"R",@"Perl",@"Lua",@"HTML",@"Scala",@"Go"];
+        languages=@[@"JavaScript",@"Java",@"PHP",@"Ruby",@"Python",@"CSS",@"C",@"Objective-C",@"Swift",@"Shell",@"R",@"Perl",@"Lua",@"HTML",@"Scala",@"Go"];
     }else if (_languageEntranceType==UserLanguageEntranceType  ) {
-    languages=@[NSLocalizedString(@"all languages", @""),@"JavaScript",@"Java",@"PHP",@"Ruby",@"Python",@"CSS",@"C",@"Objective-C",@"Shell",@"R",@"Perl",@"Lua",@"HTML",@"Scala",@"Go"];
+    languages=@[NSLocalizedString(@"all languages", @""),@"JavaScript",@"Java",@"PHP",@"Ruby",@"Python",@"CSS",@"C",@"Objective-C",@"Swift",@"Shell",@"R",@"Perl",@"Lua",@"HTML",@"Scala",@"Go"];
     
     }else if (_languageEntranceType==TrendingLanguageEntranceType ) {
         languages=@[NSLocalizedString(@"all languages", @""),@"javascript",@"java",@"php",@"ruby",@"python",@"css",@"c",@"objective-c",@"shell",@"r",@"perl",@"lua",@"html",@"scala",@"go"];
